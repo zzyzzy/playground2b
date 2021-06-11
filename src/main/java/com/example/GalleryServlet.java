4e93a64abce9b3c602f7c3f7f1053a2927f68585
zzyzzy <zzyzigy@gmail.com>
2021-06-11 23:33:24 +0900

    galleryServlet 추가

---
+++ src/main/java/com/example/GalleryServlet.java
@@ -0,0 +1,9 @@
+package com.example;
+
+/**
+ * Created by jojoldu@gmail.com on 2021-06-11
+ * Blog : http://jojoldu.tistory.com
+ * Github : http://github.com/jojoldu
+ */
+public class GalleryServlet {
+}
